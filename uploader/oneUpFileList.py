6974b65d38549dbf3f3d2224561d9c69f128ab4d
Dan Toloudis <danielt@alleninstitute.org>
2016-09-27 08:49:32 -0700

    add some more bisque uploader scripts

---
+++ uploader/oneUpFileList.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+
+try:
+    from lxml import etree
+except ImportError:
+    import xml.etree.ElementTree as etree
+
+import uploader
+
+# debug output
+printSteps = False
+
+# should the uploaded images be available to all?
+uploadPublic = True
+
+# command line args
+parser = argparse.ArgumentParser()
+# fileList = './files.txt'
+parser.add_argument("fileList", help="the file containing names of files to upload, one per line")
+# dataPath = 'file:///data/aics/software_it/danielt/images/AICS/bisque/Mito/'
+parser.add_argument("dataPath", help="the directory containing the ome tiff files on the file store")
+args = parser.parse_args()
+
+path_csv = args.fileList
+
+
+session = uploader.init()
+with open(path_csv, 'rU') as csvfile, open('out.txt', 'w') as outfile:
+    dataUrls = []
+    contents = [rw.strip() for rw in csvfile.readlines() if not rw.startswith('#')]
+    for fname in contents:
+        fullpath = args.dataPath + fname + '.ome.tif'
+        resource = etree.Element('image',
+            name=fname,
+            value=fullpath,
+            permission='published')
+        t = etree.SubElement(resource, 'tag', name='url', value=fullpath, type='link')
+        etree.SubElement(resource, 'tag', name='name', value=fname)
+        etree.SubElement(resource, 'tag', name='type', value='cellStructureModel')
+
+        resource_uniq = uploader.uploadFileSpec(session, resource, None)
+        print fname + ',' + resource_uniq
+
+        dataUrls.append(uploader.dataServiceURL + resource_uniq)
+
+        outfile.write(fname + ',' + resource_uniq + os.linesep)
--- uploader/uploader.py
+++ uploader/uploader.py
@@ -15,13 +15,17 @@ from bqapi.util import save_blob, localpath2url
 
 # THE UPLOADER.
 
+
 def init():
-    # user and password
     root = 'http://10.128.62.45:8080'
+    # user and password
     user = 'admin'
     pswd = 'admin'
     session = BQSession().init_local(user, pswd, bisque_root=root, create_mex=False)
     return session
+
+def dataServiceURL(session):
+    return session.service_url('data_service')
 
 def uploadFileSpec(session, fxml, filepath=None):
     r = etree.XML(session.postblob(filepath, xml=fxml)).find('./')
@@ -34,3 +38,18 @@ def uploadFileSpec(session, fxml, filepath=None):
         # don't store url
         # f['url'] = r.get('uri')
         return r.get('resource_uniq')
+
+
+def uploadDataSet(session, dsname, resourceIds):
+    ''' upload a named dataset comprised of a list of resourceIds '''
+    dataset = etree.Element('dataset', name=dsname)
+    for i in resourceIds:
+        v = etree.SubElement(dataset, 'value', type='object')
+        v.text = dataServiceURL+i
+    serviceurl = session.service_url('data_service')
+    response = session.postxml(serviceurl, dataset)
+    if response is None or response.get('uri') is None:
+        print 'Dataset upload failed'
+        return None
+    else:
+        print 'Uploaded Dataset ID: %s, URL: %s' % (response.get('resource_uniq'), response.get('uri'))
