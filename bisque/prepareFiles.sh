542689cae5096c95f5012cfe15a56f3d873e79a9
Dan Toloudis <danielt@alleninstitute.org>
2016-09-27 15:45:30 -0700

    initial add of a script to process and ingest images into bisque

---
+++ bisque/prepareFiles.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# assumes a naming convention for the tif files
+
+# virtualenv venv
+# source venv/bin/activate
+# pip install bisque-api
+# pip install --user dist/aicsimagetools-0.0.1-py2.py3-none-any.whl
+
+UPLOADER_PATH=~/src/aicsviztools/bisque/uploader/
+# export so that xargs sh can see it.
+export AICSIMGPATH=/Volumes/aics/software_it/danielt/images/test/
+URLIMGPATH=file:///data/aics/software_it/danielt/images/test/
+
+ls $AICSIMGPATH | grep -o 'img[0-9]\{1,3\}_[0-9]' | uniq > files.txt
+xargs -n 1 sh -c 'interleave.py --path "$AICSIMGPATH" --outpath "$AICSIMGPATH" --prefix $0 || exit 255' < files.txt
+python $UPLOADER_PATH/oneUpFileList.py ./files.txt $URLIMGPATH
+rm files.txt
+mv out.txt outTest.txt
+
+# AICSIMGPATH=/Volumes/aics/software_it/danielt/images/AICS/Mito/
+# URLIMGPATH=file:///data/aics/software_it/danielt/images/AICS/bisque/Mito/
+#
+# ls $AICSIMGPATH | grep -o 'img[0-9]\{1,3\}_[0-9]' | uniq > files.txt
+# xargs -n 1 sh -c 'python aicsimagetools/bin/interleave.py --path $AICSIMGPATH --outpath $AICSIMGPATH --prefix $0 || exit 255' < files.txt
+# python aicsviztools/bisque/uploader/oneUpFileList.py --fileList ./files.txt --dataPath $URLIMGPATH
+# mv out.txt outMito.txt
+#
+# AICSIMGPATH=/Volumes/aics/software_it/danielt/images/AICS/alphactinin/
+# URLIMGPATH=file:///data/aics/software_it/danielt/images/AICS/bisque/alphactinin/
+#
+# ls $AICSIMGPATH | grep -o 'img[0-9]\{1,3\}_[0-9]' | uniq > files.txt
+# xargs -n 1 sh -c 'python aicsimagetools/bin/interleave.py --path $AICSIMGPATH --outpath $AICSIMGPATH --prefix $0 || exit 255' < files.txt
+# python aicsviztools/bisque/uploader/oneUpFileList.py --fileList ./files.txt --dataPath $URLIMGPATH
+# mv out.txt outAlphactinin.txt
+#
+# AICSIMGPATH=/Volumes/aics/software_it/danielt/images/AICS/tub/
+# URLIMGPATH=file:///data/aics/software_it/danielt/images/AICS/bisque/tub/
+#
+# ls $AICSIMGPATH | grep -o 'img[0-9]\{1,3\}_[0-9]' | uniq > files.txt
+# xargs -n 1 sh -c 'python aicsimagetools/bin/interleave.py --path $AICSIMGPATH --outpath $AICSIMGPATH --prefix $0 || exit 255' < files.txt
+# python aicsviztools/bisque/uploader/oneUpFileList.py --fileList ./files.txt --dataPath $URLIMGPATH
+# mv out.txt outTub.txt
