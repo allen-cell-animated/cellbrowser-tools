b86005f6032bfbd47400bae5ed63e35cccd71b95
dmt <danielt@alleninstitute.org>
2017-02-15 15:38:42 -0800

    add db counting command, dbstat

---
+++ uploader/dbstat.py
@@ -0,0 +1,29 @@
+import argparse
+import db_api
+import db_ops
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='input name query')
+    args = parser.parse_args()
+
+    session_dict = {
+        # 'root': 'http://test-aics-01',
+        # 'root': 'http://bisque-00.corp.alleninstitute.org:8080',
+        # 'root': 'http://10.128.62.104:8080',
+        'root': 'http://10.128.62.104',
+        'user': 'admin',
+        'password': 'admin'
+    }
+
+    db_api.DbApi.setSessionInfo(session_dict)
+    # db_ops.countImagesByName(args.input)
+    xml = db_api.DbApi.getImagesByTagValue("isCropped", "true")
+    print 'Retrieved ' + str(len(xml.getchildren())) + ' images.'
+
+if __name__ == "__main__":
+    print (sys.argv)
+    main()
+    sys.exit(0)
