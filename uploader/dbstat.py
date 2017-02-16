60759747bb5001f05814d90ec46981a8f9dd6085
dmt <danielt@alleninstitute.org>
2017-02-16 11:12:41 -0800

    add utility scripts to help validation of data set

--- uploader/dbstat.py
+++ uploader/dbstat.py
@@ -21,7 +21,44 @@ def main():
     db_api.DbApi.setSessionInfo(session_dict)
     # db_ops.countImagesByName(args.input)
     xml = db_api.DbApi.getImagesByTagValue("isCropped", "true")
-    print 'Retrieved ' + str(len(xml.getchildren())) + ' images.'
+    print 'Retrieved ' + str(len(xml.getchildren())) + ' images with "isCropped" true.'
+    srcdict = {}
+    for element in xml:
+        child_el = element.find('tag[@name="source"]')
+        src = child_el.attrib['value']
+        # TODO find a nicer way to get this grouping.
+        # strip away all after the last underscore.
+        if src.startswith('35000'):
+            src = src[:src.index('_')]
+        else:
+            src = src[:src.rindex('_')]
+        # add to dict
+        if src not in srcdict:
+            srcdict[src] = 1
+        else:
+            srcdict[src] += 1
+    for (key, value) in sorted(srcdict.items()):
+        print(key+'\t'+str(value))
+
+    xml = db_api.DbApi.getImagesByTagValue("isCropped", "false")
+    print 'Retrieved ' + str(len(xml.getchildren())) + ' images with "isCropped" false.'
+    srcdict = {}
+    for element in xml:
+        child_el = element.find('tag[@name="name"]')
+        src = child_el.attrib['value']
+        # TODO find a nicer way to get this grouping.
+        # strip away all after the last underscore.
+        if src.startswith('35000'):
+            src = src[:src.index('_')]
+        else:
+            src = src[:src.rindex('_')]
+        # add to dict
+        if src not in srcdict:
+            srcdict[src] = 1
+        else:
+            srcdict[src] += 1
+    for (key, value) in sorted(srcdict.items()):
+        print(key+'\t'+str(value))
 
 if __name__ == "__main__":
     print (sys.argv)
