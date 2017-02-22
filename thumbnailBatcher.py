b23eead1b8ea9ce6cbaca6bbfd3e88147d0a687d
zacharyc <zacharyc@alleninstitute.org>
2017-02-22 10:43:14 -0800

    Random argument selects 20 random files from directory.

--- thumbnailBatcher.py
+++ thumbnailBatcher.py
@@ -3,7 +3,9 @@ import sys
 from os import listdir, makedirs
 from os.path import isfile, join, exists
 import re
+import random
 import thumbnailGenerator
+from glob import iglob
 from aicsimagetools import pngWriter, omeTifReader
 
 # Author: Zach Crabtree zacharyc@alleninstitute.org
@@ -14,6 +16,7 @@ _rgb = ([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]], "rgb")
 _rbg = ([[1.0, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 1.0, 0.0]], "rbg")
 _brg = ([[0.0, 0.0, 1.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]], "brg")
 color_choices = [_cym, _cmy, _rgb, _rbg, _brg]
+
 
 def full_fields_color(ome_tif_files, color):
 
@@ -39,16 +42,29 @@ def main():
     parser = argparse.ArgumentParser(description="Create new batches of thumbnails for testing")
 
     parser.add_argument('input', help="directory to search in")
-    # parser.add_argument('--random', help="generate thumbnails from random files")
+    parser.add_argument('--random', '-r', help="generate thumbnails for 20 random files", default=False, action='store_true')
 
     args = parser.parse_args()
 
-    only_files = [join(args.input, file_name) for file_name in listdir(args.input) if isfile(join(args.input, file_name))]
-    only_ome_tif = [f for f in only_files if f.endswith(".ome.tif")]
+    only_ome_tif = [f for f in iglob(join(args.input, "**", "*.ome.tif"))]
     print(only_ome_tif)
+
+    if args.random:
+        if len(only_ome_tif) <= 20:
+            random_file_list = only_ome_tif
+        else:
+            random_file_list = only_ome_tif[:21]
+            k = 1
+            for ome_tif in only_ome_tif:
+                if random.uniform(0, 21) > 21 / k:
+                    random_file_list [int(random.uniform(0, 21))] = ome_tif
+                k += 1
+        file_list = random_file_list
+    else:
+        file_list = only_ome_tif
     # TODO: RGB palettes get too much cut out, the threshold is much too high
     for color in color_choices:
-        full_fields_color(only_ome_tif, color=color)
+        full_fields_color(file_list, color=color)
 
 
 if __name__ == "__main__":
