5f2efe0de3dc64720b3ee4b5801c62718817898c
dmt <danielt@alleninstitute.org>
2017-03-13 08:56:53 -0700

    modify script to split files into groups based on experimentID per aics cell line

--- createZips.py
+++ createZips.py
@@ -1,4 +1,5 @@
 # import argparse
+import csv
 import glob
 import os
 import pandas as pd
@@ -21,6 +22,47 @@ def make_zips(dataset_name):
             zf.write(imgtozip, image)
         zf.close()
 
+def make_zips2(dataset_name):
+    # plan: read from delivery_summary based on "dataset" arg
+    # delivery_summary contains rows listing all the csv files to load
+    datadir = './data/' + dataset_name
+    aicsnum_index = {}
+    with open(datadir + '/delivery_summary.csv', 'rU') as summarycsvfile:
 
-make_zips('nuc_cell_seg_delivery_20170210')
-make_zips('nuc_cell_seg_delivery_20170217')
+        # every cell I process will get a line in this file.
+        cellnamemapfilename = datadir + '/cellnames.csv'
+        cellnamemapfile = open(cellnamemapfilename, 'rU')
+        cellnamemapreader = csv.reader(cellnamemapfile)
+        cellnamemap = {rows[1]:rows[0] for rows in cellnamemapreader}
+
+        summaryreader = csv.DictReader(summarycsvfile)
+        summary_first_field = summaryreader.fieldnames[0]
+        for summaryrow in summaryreader:
+            if summaryrow[summary_first_field].startswith("#"):
+                continue
+            if summaryrow[summary_first_field] == "":
+                continue
+
+            # i will make one zip file for each eid.
+            # the zip file's name will be the aics number plus a _partX index.
+            eid = summaryrow['Experiment ID']
+            aicsnum = summaryrow['AICS-#']
+
+            # find all images with eid from the cellnamemap
+            # eid+'_'
+            imagenames = [(aicsnum + '/' + cellnamemap[key] + '.ome.tif') for key in cellnamemap if key.startswith(eid)]
+
+            # output subdirectory name
+            # subdir = aicsnum
+
+            if aicsnum in aicsnum_index:
+                aicsnum_index[aicsnum] += 1
+            else:
+                aicsnum_index[aicsnum] = 0
+
+            thefile = open(datadir+'/'+aicsnum+'_part'+str(aicsnum_index[aicsnum])+'.txt', 'wb')
+            thefile.write('\n'.join(imagenames))
+            thefile.close()
+
+
+make_zips2('2017_03_08_Struct_First_Pass_Seg')
