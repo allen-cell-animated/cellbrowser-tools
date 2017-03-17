e51aabf85e53ac4a27bcc54038c78e62b66e4fbc
dmt <danielt@alleninstitute.org>
2017-03-17 15:11:29 -0700

    add another type of metadata string fixup to replaceChannelNames.sh

--- replaceChannelNames.sh
+++ replaceChannelNames.sh
@@ -2,6 +2,7 @@
 
 echo $1
 # uses bioformats command line tool tiffcomment
-./tiffcomment $1 | sed -e 's/MEMB:0/MEMB/g;s/STRUCT:1/STRUCT/g;s/DNA:2/DNA/g;s/TRANS:3/TRANS/g;s/SEG_NUC:4/SEG_DNA/g;s/SEG_CELL:5/SEG_MEMB/g;s/SEG
-_STRUCT:6/SEG_STRUCT/g' > tmp.xml
+# ./tiffcomment $1 | sed -e 's/MEMB:0/MEMB/g;s/STRUCT:1/STRUCT/g;s/DNA:2/DNA/g;s/TRANS:3/TRANS/g;s/SEG_NUC:4/SEG_DNA/g;s/SEG_CELL:5/SEG_MEMB/g;s/SEG_STRUCT:6/SEG_STRUCT/g' > tmp.xml
+# replace SEG_STRUCT with CON_Memb, and THEN replace SEG_DNA with SEG_STRUCT
+./tiffcomment $1 | sed -e 's/SEG_STRUCT/CON_Memb/g;s/SEG_DNA/SEG_STRUCT/g' > tmp.xml
 ./tiffcomment -set tmp.xml $1
