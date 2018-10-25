f48e63435ee1c795fbd34777f08cd9a02e044e20
dmt <danielt@alleninstitute.org>
2018-10-25 16:34:59 -0700

    rename cell-line-def

--- generateCellLineDef.py
+++ generateCellLineDef.py
@@ -122,7 +122,7 @@ def use_select_rows_cellline_name_to_protein_name(server, prefs):
     log.debug(PP.pformat(rows))
     log.debug("Row Count {}: ".format(len(rows)))
     import json
-    with open(os.path.join(prefs.get("out_status"), 'cellLineDef.json'), 'w') as outfile:
+    with open(os.path.join(prefs.get("out_status"), 'cell-line-def.json'), 'w') as outfile:
         json.dump(rows, outfile, indent=4)
 
 def use_execute_sql_cellline_name_to_protein_name(server):
