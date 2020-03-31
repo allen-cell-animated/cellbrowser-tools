08a84446a81252f4eb72d5689412809770354bbe
Daniel Toloudis <danielt@alleninstitute.org>
2020-03-31 10:41:31 -0700

    validate expected added columns

--- cellbrowser_tools/dataHandoffUtils.py
+++ cellbrowser_tools/dataHandoffUtils.py
@@ -158,17 +158,6 @@ def collect_data_rows(fovids=None, raw_only=False, max_rows=None):
     )
     print("GOT MITOTIC ANNOTATIONS")
 
-    extra_columns = [
-        "IsMitotic",
-        "MitoticState",
-        "LegacyCellName",
-        "LegacyFOVName",
-        "Angle",
-        "x",
-        "y",
-        "FOV_3dcv_Name",
-    ]
-
     mitoticdata = pd.DataFrame(mitoticdata)
     mitoticbooldata = mitoticdata[mitoticdata["MitoticStateId/Name"] == "Mitosis"]
     mitoticstatedata = mitoticdata[mitoticdata["MitoticStateId/Name"] != "Mitosis"]
@@ -291,6 +280,21 @@ def collect_data_rows(fovids=None, raw_only=False, max_rows=None):
     print("DONE BUILDING TABLES")
 
     print(list(df_data_handoff.columns))
+
+    extra_columns = [
+        "IsMitotic",
+        "MitoticState",
+        "LegacyCellName",
+        "LegacyFOVName",
+        "Angle",
+        "x",
+        "y",
+        "FOV_3dcv_Name",
+    ]
+    # verify the expected column names in the above query
+    for field in extra_columns:
+        if field not in df_data_handoff.columns:
+            raise f"Expected {field} to be in cellbrowser-tools dataset results."
 
     return df_data_handoff
 
