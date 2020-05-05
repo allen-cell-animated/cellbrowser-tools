ffa1e27bc1a8b55b450d4219660ea003c7bf5771
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-05-05 13:06:20 -0700

    More cluster config permutations

--- cellbrowser_tools/bin/build_release.py
+++ cellbrowser_tools/bin/build_release.py
@@ -79,7 +79,7 @@ def process_fov_rows(groups, args, prefs, distributed_executor_address):
             [g for g in groups],
             [args for g in groups],
             [prefs for g in groups],
-            batch_size=30,
+            batch_size=20,
         )
     return "Done"
 
@@ -134,8 +134,8 @@ def select_dask_executor(p, prefs):
             # Create cluster
             log.info("Creating SLURMCluster")
             cluster = SLURMCluster(
-                cores=4,
-                memory="20GB",
+                cores=12,
+                memory="40GB",
                 queue="aics_cpu_general",
                 walltime="10:00:00",
                 local_directory=str(log_dir),
@@ -144,7 +144,7 @@ def select_dask_executor(p, prefs):
             log.info("Created SLURMCluster")
 
             # Set worker scaling settings
-            cluster.scale(100)
+            cluster.scale(20)
 
             # Use the port from the created connector to set executor address
             distributed_executor_address = cluster.scheduler_address
