9ef67a15beda1c2c36576efe5e698f91a9af23a6
Daniel Toloudis <danielt@alleninstitute.org>
2022-12-02 15:27:58 -0800

    try endpoint_url

--- cellbrowser_tools/zarr_fov_processing.py
+++ cellbrowser_tools/zarr_fov_processing.py
@@ -135,7 +135,14 @@ class ImageProcessor:
         # note need aws creds locally for this to work
         os.environ["AWS_PROFILE"] = "animatedcell"
         os.environ["AWS_DEFAULT_REGION"] = "us-west-2"
-        s3 = s3fs.S3FileSystem(anon=False, config_kwargs={"connect_timeout": 60})
+        s3 = s3fs.S3FileSystem(
+            anon=False,
+            config_kwargs={
+                "connect_timeout": 60,
+                "endpoint_url": "https://s3.us-west-2.amazonaws.com",
+            },
+        )
+
         # cluster = LocalCluster(n_workers=4, processes=True, threads_per_worker=1)
         # client = Client(cluster)
 
