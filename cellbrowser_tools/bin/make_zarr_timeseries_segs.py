58fd30d463303fd17c460e0e5583f32cedf9ae5c
Daniel Toloudis <danielt@alleninstitute.org>
2024-01-25 08:22:00 -0800

    many changes trying various methods

--- cellbrowser_tools/bin/make_zarr_timeseries_segs.py
+++ cellbrowser_tools/bin/make_zarr_timeseries_segs.py
@@ -1,8 +1,12 @@
 # allow for dask parallelism
-from distributed import LocalCluster, Client
+from dask.distributed import Client, LocalCluster
 
 import os
-from typing import Tuple
+from typing import Tuple, Any
+import atexit
+import signal
+import sys
+from pathlib import Path
 
 # import pathlib
 # from pathlib import Path
@@ -16,11 +20,31 @@ from bioio_czi import Reader as CziReader
 from bioio_ome_tiff import Reader as TiffReader
 from aicsfiles import fms, FileLevelMetadataKeys
 
-# import numpy
+import numpy as np
 import pandas
 import dask
+from dask import array as da
+from zarr.storage import DirectoryStore, FSStore
+import zarr
+import skimage
 
 from cellbrowser_tools.dataHandoffUtils import normalize_path
+
+from ngff_zarr import config, to_ngff_image, to_multiscales, to_ngff_zarr, Methods
+from ngff_zarr.rich_dask_progress import NgffProgress
+from rich.console import Console
+from rich.live import Live
+from rich.panel import Panel
+from rich.pretty import Pretty
+from rich.progress import (
+    MofNCompleteColumn,
+    SpinnerColumn,
+    TimeElapsedColumn,
+)
+from rich.progress import (
+    Progress as RichProgress,
+)
+from rich.spinner import Spinner
 
 # from ome_zarr.scale import Scaler
 
@@ -187,6 +211,61 @@ def convert_to_zarr(
             )
 
 
+def resize(
+    image: da.Array, output_shape: Tuple[int, ...], *args: Any, **kwargs: Any
+) -> da.Array:
+    r"""
+    Wrapped copy of "skimage.transform.resize"
+    Resize image to match a certain size.
+    :type image: :class:`dask.array`
+    :param image: The dask array to resize
+    :type output_shape: tuple
+    :param output_shape: The shape of the resize array
+    :type \*args: list
+    :param \*args: Arguments of skimage.transform.resize
+    :type \*\*kwargs: dict
+    :param \*\*kwargs: Keyword arguments of skimage.transform.resize
+    :return: Resized image.
+    """
+    factors = np.array(output_shape) / np.array(image.shape).astype(float)
+    # Rechunk the input blocks so that the factors achieve an output
+    # blocks size of full numbers.
+    better_chunksize = tuple(
+        np.maximum(1, np.round(np.array(image.chunksize) * factors) / factors).astype(
+            int
+        )
+    )
+    image_prepared = image.rechunk(better_chunksize)
+
+    # If E.g. we resize image from 6675 by 0.5 to 3337, factor is 0.49992509 so each
+    # chunk of size e.g. 1000 will resize to 499. When assumbled into a new array, the
+    # array will now be of size 3331 instead of 3337 because each of 6 chunks was
+    # smaller by 1. When we compute() this, dask will read 6 chunks of 1000 and expect
+    # last chunk to be 337 but instead it will only be 331.
+    # So we use ceil() here (and in resize_block) to round 499.925 up to chunk of 500
+    block_output_shape = tuple(
+        np.ceil(np.array(better_chunksize) * factors).astype(int)
+    )
+
+    # Map overlap
+    def resize_block(image_block: da.Array, block_info: dict) -> da.Array:
+        # if the input block is smaller than a 'regular' chunk (e.g. edge of image)
+        # we need to calculate target size for each chunk...
+        chunk_output_shape = tuple(
+            np.ceil(np.array(image_block.shape) * factors).astype(int)
+        )
+        return skimage.transform.resize(
+            image_block, chunk_output_shape, *args, **kwargs
+        ).astype(image_block.dtype)
+
+    output_slices = tuple(slice(0, d) for d in output_shape)
+    output = da.map_blocks(
+        resize_block, image_prepared, dtype=image.dtype, chunks=block_output_shape
+    )[output_slices]
+    return output.rechunk(image.chunksize).astype(image.dtype)
+
+
+
 if __name__ == "__main__":
     ###########################
     # Infrastructure init
@@ -197,8 +276,31 @@ if __name__ == "__main__":
     # initialize for writing direct to S3
     s3 = s3fs.S3FileSystem(anon=False, config_kwargs={"connect_timeout": 60})
     # configure for parallelism
-    cluster = LocalCluster(n_workers=4, processes=True, threads_per_worker=1)
+
+    n_workers = 4
+    worker_memory_target = config.memory_target // n_workers
+    try:
+        import psutil
+        n_workers = psutil.cpu_count(False) // 2
+        worker_memory_target = config.memory_target // n_workers
+    except ImportError:
+        pass
+
+    cluster = LocalCluster(
+        n_workers=n_workers,
+        memory_limit=worker_memory_target,
+        processes=True,
+        threads_per_worker=2,
+    )
     client = Client(cluster)
+
+    def shutdown_client(sig_id, frame):  # noqa: ARG001
+        client.shutdown()
+
+    atexit.register(shutdown_client, None, None)
+    signal.signal(signal.SIGTERM, shutdown_client)
+    signal.signal(signal.SIGINT, shutdown_client)
+
     print(client)
     print(client.dashboard_link)
 
@@ -266,6 +368,8 @@ if __name__ == "__main__":
     print(str(im3.dims.Y))
     print(str(im3.dims.Z))
 
+    numT = len(seg_paths)  # min(len(seg_paths), 2)
+
     # make dask chunks large.
     # dask best practices say to use at least 100MB per chunk.
     # but also want to keep chunk size a multiple of our
@@ -278,25 +382,31 @@ if __name__ == "__main__":
         DimensionNames.Samples,
     ]
     zarr_chunk_dims = []
+    zarr_chunk_dims_lists = []
     # TODO - allow different zarr chunk dims for each downsample
     # TODO - bioio input chunk dims are too coarse-grained
-    nlevels = 4
-    scaling = [1, 1, 1, 0.5, 0.5]
+    nlevels = 5
+    scale_factor = 2.0
+    scaling = [1, 1, 1, 1.0/scale_factor, 1.0/scale_factor]
+    inv_scaling = [1,1,1,scale_factor,scale_factor]
     for i in range(nlevels):
         zarr_chunk_dims.append(
-            [
-                1,
-                1,
-                (int((1 / scaling[3]) * (1 / scaling[4])) ** i),
-                im3.dims.Y,
-                im3.dims.X,
-            ]
+            {
+                "t":1,
+                "c":1,
+                "z":(int(inv_scaling[3] * inv_scaling[4]) ** i),
+                "y":int(im3.dims.Y * (scaling[3]**i)),
+                "x":int(im3.dims.X * (scaling[4]**i)),
+            }
+        )
+        zarr_chunk_dims_lists.append(
+            [1,1,(int(inv_scaling[3] * inv_scaling[4]) ** i),int(im3.dims.Y * (scaling[3]**i)),int(im3.dims.X * (scaling[4]**i))]
         )
 
     # load all data into a nice big delayed array
     data = []
 
-    for i in range(min(len(seg_paths), 30)):
+    for i in range(numT):
         im = BioImage(raw_paths[i], reader=TiffReader, chunk_dims=bioio_chunk_dims)
         data_raw = im.get_image_dask_data("CZYX")
         data.append(data_raw)
@@ -308,8 +418,42 @@ if __name__ == "__main__":
     data = dask.array.stack(data)
     print("Dask array has been built")
     print(data.shape)
-    # output_bucket = "animatedcell-test-data"
-    # convert_to_zarr(filepath, output_bucket, channel_selection=channelinds)
+
+    output_bucket = "animatedcell-test-data"
+    output_filename = os.path.splitext(os.path.basename(filepath))[0]
+
+    # console = Console()
+    # progress = RichProgress(
+    #     SpinnerColumn(),
+    #     MofNCompleteColumn(),
+    #     TimeElapsedColumn(),
+    #     *RichProgress.get_default_columns(),
+    #     transient=False,
+    #     console=console,
+    # )
+    # rich_dask_progress = NgffProgress(progress)
+
+    # ngff_image = to_ngff_image(data,
+    #                            dims= ("t", "c", "z", "y", "x"),
+    #                            scale = {"c":1, "t":1, "x":im3.physical_pixel_sizes.X, "y":im3.physical_pixel_sizes.Y, "z":im3.physical_pixel_sizes.Z},
+    #                            translation = None,
+    #                            name = "image",
+    #                            axes_units = None
+    # )
+    # multiscales = to_multiscales(
+    #     ngff_image,
+    #     method=Methods.DASK_IMAGE_NEAREST,
+    #     progress=rich_dask_progress,
+    #     chunks=zarr_chunk_dims[0],
+    #     cache= (ngff_image.data.nbytes > config.memory_target),
+    #     scale_factors=[{"x":2, "y":2, "z":1, "t":1, "c":1}]
+    # )
+    # output_store = DirectoryStore(f"s3://{output_bucket}/{output_filename}_with_seg.zarr", dimension_separator="/")
+    # to_ngff_zarr(
+    #     output_store,
+    #     multiscales,
+    #     progress=rich_dask_progress
+    # )
 
     # construct some per-channel lists to feed in to the writer.
     # hardcoding to 2 for now
@@ -317,17 +461,55 @@ if __name__ == "__main__":
         0xFF0000,
         # 0x00FF00
     ]
-    output_bucket = "animatedcell-test-data"
-    output_filename = os.path.splitext(os.path.basename(filepath))[0]
-    writer = OmeZarrWriter(f"s3://{output_bucket}/{output_filename}_with_seg.zarr/")
+    channel_names = ["raw"]  # , "seg"]
 
-    writer.write_image(
-        image_data=data,
-        image_name="",
-        physical_pixel_sizes=im3.physical_pixel_sizes,
-        channel_names=["raw"],  # , "seg"
-        channel_colors=channel_colors,
-        scale_num_levels=4,
-        scale_factor=2.0,
-        chunk_dims=zarr_chunk_dims,
-    )
+    # writer = OmeZarrWriter(f"s3://{output_bucket}/{output_filename}_with_seg.zarr/")
+    # writer.write_image(
+    #     image_data=data,
+    #     image_name="",
+    #     physical_pixel_sizes=im3.physical_pixel_sizes,
+    #     channel_names=channel_names,
+    #     channel_colors=channel_colors,
+    #     scale_num_levels=nlevels,
+    #     scale_factor=scale_factor,
+    #     chunk_dims=zarr_chunk_dims_lists,
+    # )
+
+    # init zarr store as a group.
+    # each multiresolution level will be a zarr array
+    # but we will write into each group by breadth
+    # loop over T
+    # for each t in T
+    #   load level 0 as a dask array
+    #   write level 0 to zarr group 0 at level=0 / T=t
+    #   downsample and write other levels
+
+    output_store = FSStore(url=f"s3://{output_bucket}/{output_filename}_with_seg.zarr", dimension_separator="/")
+    # output_store = DirectoryStore(f"c:/{output_bucket}/{output_filename}_with_seg.zarr", dimension_separator="/")
+    # create a group with all the levels
+    root = zarr.group(store=output_store, overwrite=True)
+    # set up levels
+    lvl_shape = data.shape  # (numT,1,148,2047,848)
+    lvls = []
+    for i in range(nlevels):
+        lvl = root.zeros(str(i), shape=lvl_shape, chunks=zarr_chunk_dims_lists[i])
+        lvls.append(lvl)
+        lvl_shape = (lvl_shape[0]*scaling[0], lvl_shape[1]*scaling[1], lvl_shape[2]*scaling[2], lvl_shape[3]*scaling[3], lvl_shape[4]*scaling[4])
+        lvl_shape = (int(lvl_shape[0]), int(lvl_shape[1]), int(lvl_shape[2]), int(lvl_shape[3]), int(lvl_shape[4]))
+
+    for i in range(numT):
+        ti = data[i]
+        # ti is level0's CZYX data. 
+        # we can write it right now and then downsample
+        for j in range(nlevels):
+            ti = ti.persist()
+            ti.compute()
+            lvls[j][i] = ti
+            # downsample to next level
+            nextshape = (int(ti.shape[0]), int(ti.shape[1]), int(ti.shape[2]/scale_factor), int(ti.shape[3]/scale_factor))
+            ti = resize(ti, nextshape, order=0)
+            # ti = dask.array.coarsen(dask.array.mean, ti, {0:inv_scaling[1], 1:inv_scaling[2], 2:inv_scaling[3], 3:inv_scaling[4]})
+            ti = ti.astype("uint16")
+    # # now the outer list data is dimension T and the inner items are all CZYX
+    # data = dask.array.stack(data)
+
